eaa0db8440be9dc7a6615deadc96b4348cfd435d
Matteo <teo.mastropasqua@gmail.com>
2025-01-11 13:18:06 +0100

    .

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,9 +5,9 @@ const NavBar = () => {
     return (
     <>
         <nav className="navbar mb-lg-5 mb-4 d-flex justify-content-evenly text-center mastro-orange fs-5">
-            <Link to='/AboutMe'     className="nav-link p-2">About Me</Link>
-            <Link to='/Projects'    className="nav-link p-2">Projects</Link>
-            <Link to='/Contacts'    className="nav-link p-2">Contacts</Link>
+            <Link to='/AboutMe'  className="nav-link p-2">About Me</Link>
+            <Link to='/Projects' className="nav-link p-2">Projects</Link>
+            <Link to='/Contacts' className="nav-link p-2">Contacts</Link>
         </nav>
     </>
     )
