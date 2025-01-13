6097e7fd0f68245789040771ecee17a2fcaddf89
Matteo <teo.mastropasqua@gmail.com>
2025-01-13 11:27:51 +0100

    HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Header from './components/Header'
 import ImagePopUp from './components/ImagePopUp'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
 import NavBar from './components/NavBar'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './components/Mastro.css'
+import CardGroup from './components/CardGroups'
+import Contacts from './components/Contacts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter basename='/mastrofolio'>
+  <HashRouter>
     <Header>Matteo Mastropasqua</Header>
     <NavBar></NavBar>
     <div className='d-flex justify-content-center'>
       <div className='w-lg-75 w-sm-100'>
-        <App/>
-        {/* <Routes>
-          <Route index             element={<App        />} />
-          <Route path='Projects'   element={<Projects   />} />
-          <Route path="AboutMe"    element={<AboutMe    />} />
-          <Route path="ImagePopUp" element={<ImagePopUp />} />
-        </Routes> */}
+          <Routes>
+            <Route index element={<CardGroup/>}/>
+            <Route path='Projects' element={<Projects/>}/>
+            <Route path="AboutMe" element={<AboutMe/>}/>
+            <Route path="Contacts" element={<Contacts />}/>
+            <Route Component={CardGroup} />
+          </Routes>
       </div>
     </div>
-  </BrowserRouter>,
+  </HashRouter>,
 )
