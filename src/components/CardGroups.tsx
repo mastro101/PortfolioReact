1c8ef0c6fb497333d585bd444628e59f4e0deca4
Matteo <teo.mastropasqua@gmail.com>
2025-02-02 18:48:18 +0100

    rework CardGroups

--- src/components/CardGroups.tsx
+++ src/components/CardGroups.tsx
@@ -6,17 +6,28 @@ const CardGroup = () => {
 
     const text1 = "i'm a duck"
 
+    const progects = [];
+    for(let i = 0; i < data.projects.length; i++)
+    {
+        let currentProject = data.projects[i];
+        if (currentProject.active == false)
+            continue;
+
+        progects.push(
+            <Card myData={currentProject}/>
+        );
+        
+        if (progects.length >= 5)
+            break;
+    }
+
     return (
     <>
         
         {/* <iframe className="w-100" style={{height: "650px"}} src="https://html-classic.itch.zone/html/12515981/Portfolio/index.html" title="Mastrofolio" allowFullScreen></iframe> */}
 
         <div className="card-group">
-            <Card myData={data.projects[0]}/>
-            <Card myData={data.projects[2]}/>
-            <Card myData={data.projects[3]}/>
-            <Card myData={data.projects[4]}/>
-            <Card myData={data.projects[5]}/>
+            {progects}
         </div>
     </>
     )
